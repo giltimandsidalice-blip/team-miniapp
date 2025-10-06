d7c7248338c2abf2a988334c8d47f6b061dcc5b3
giltimandsidalice-blip <giltimandsidalice@gmail.com>
2025-10-06 17:54:30 +0300

    Update diag.js

--- api/diag.js
+++ api/diag.js
@@ -1,5 +1,6 @@
-// /api/diag.js  (remove later)
-export default async function handler(req, res) {
+// /api/diag.js
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.json({
     openai_key_present: !!process.env.OPENAI_API_KEY,
     openai_model: process.env.OPENAI_MODEL || "gpt-4o-mini",
