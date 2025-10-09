beb3caf12aa71ae26d01ee9aa1bbc9e641fdb832
giltimandsidalice-blip <giltimandsidalice@gmail.com>
2025-10-09 13:16:05 +0300

    Update send-message.js

--- api/send-message.js
+++ api/send-message.js
@@ -1,3 +1,6 @@
+import { verifyTelegramInitData } from "../_utils/verifyTelegram.js";
+import { sendToTelegram } from "../_utils/sendToTelegram.js";
+
 export default async function handler(req, res) {
   try {
     if (req.method !== "POST") {
