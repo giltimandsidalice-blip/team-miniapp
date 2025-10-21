61447131ff476a50361b4b7bd0d3df70cc8ead74
giltimandsidalice-blip <giltimandsidalice@gmail.com>
2025-10-21 19:10:43 +0300

    Update cached-chats.js

--- api/cached-chats.js
+++ api/cached-chats.js
@@ -1,6 +1,6 @@
 // api/cached-chats.js
 
-import { getSupabase } from "./_utils/supabase";
+import { getSupabase } from "./_utils/supabase.js";
 
 export default async function handler(req, res) {
   res.setHeader("Content-Type", "application/json; charset=utf-8");
