1551315e9e4c84ed0606b89dcd4914a4018b82b9
giltimandsidalice-blip <giltimandsidalice@gmail.com>
2025-10-21 18:58:27 +0300

    Update test-connection.js

--- api/test-connection.js
+++ api/test-connection.js
@@ -1,4 +1,4 @@
-import { getSupabase } from "@/api/_utils/supabase";
+import { getSupabase } from "./_utils/supabase";
 
 export default async function handler(req, res) {
   const usernameHeader = req.headers['x-telegram-username'];
